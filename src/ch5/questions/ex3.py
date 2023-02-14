578ddadb804f7eb5fdf526ab73b67a6fe2328b3b
Yuta NAKATA <n-yuta@wni.com>
2023-02-14 20:53:07 +0900

    add : 第５章を追加

---
+++ src/ch5/questions/ex3.py
@@ -0,0 +1,12 @@
+from cards import Card
+
+def test_finish(cards_db):
+    for c in [
+        Card("write a book", state='done'),
+        Card('second edition', state="in prog"),
+        Card('create a course', state='todo')
+    ]:
+        index = cards_db.add_card(c)
+        cards_db.start(index)
+        card = cards_db.get_card(index)
+        assert card.state == 'in prog'
