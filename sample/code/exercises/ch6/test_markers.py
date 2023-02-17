d56bda11959e11d6b8f2e244464297c0da09ab18
中田裕太 <n-yuta@MacBookAir-of-Ny.local>
2023-02-17 21:59:18 +0900

    第６章練習問題を実施

--- sample/code/exercises/ch6/test_markers.py
+++ sample/code/exercises/ch6/test_markers.py
@@ -1,6 +1,8 @@
 import pytest
 
+pytestmark = pytest.mark.all
 
+@pytest.mark.odd
 def test_one():
     pass
 
@@ -8,19 +10,22 @@ def test_one():
 def test_two():
     pass
 
-
+@pytest.mark.odd
 def test_three():
     pass
 
 
+@pytest.mark.testclass
 class TestClass:
     def test_four(self):
         pass
 
+    @pytest.mark.odd
     def test_five(self):
         pass
 
-
+@pytest.mark.odd(7)
 @pytest.mark.parametrize("x", [6, 7])
 def test_param(x):
+    print(x)
     pass
