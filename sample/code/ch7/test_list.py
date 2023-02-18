62b26e8059bd41401f08a4d293fec2295aa6080f
中田裕太 <n-yuta@MacBookAir-of-Ny.local>
2023-02-18 17:26:21 +0900

    add : 第９章の内容を追加

--- sample/code/ch7/test_list.py
+++ sample/code/ch7/test_list.py
@@ -29,3 +29,49 @@ def test_list_several_cards(cards_db):
     assert len(the_list) == len(orig)
     for c in orig:
         assert c in the_list
+
+
+def test_one(cards_db):
+    orig = [
+        Card("foo"),
+        Card("bar", owner="me"),
+        Card("baz", owner="you", state="in prog"),
+    ]
+
+    for c in orig:
+        cards_db.add_card(c)
+
+    for c in orig:
+        for c2 in cards_db.list_cards(owner=c.owner):
+            assert c.owner == c2.owner
+
+
+def test_two(cards_db):
+    orig = [
+        Card("foo"),
+        Card("bar", owner="me"),
+        Card("baz", owner="you", state="in prog"),
+    ]
+
+    for c in orig:
+        cards_db.add_card(c)
+
+    for c in orig:
+        for c2 in cards_db.list_cards(state=c.state):
+            assert c.state == c2.state
+
+
+
+def test_three(cards_db):
+    orig = [
+        Card("foo"),
+        Card("bar", owner="me"),
+        Card("baz", owner="you", state="in prog"),
+    ]
+
+    for c in orig:
+        cards_db.add_card(c)
+
+    for c in orig:
+        for c2 in cards_db.list_cards(state=c.state, owner=c.owner):
+            assert c.state == c2.state and c.owner == c2.owner
